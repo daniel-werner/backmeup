e9bc2f29799ca8ab64aaab8b738411994a5d18f4
Daniel Werner <vernerd@gmail.com>
2019-12-25 17:14:58 +0100

    Tar dereference symlinks

--- bin/backup.sh
+++ bin/backup.sh
@@ -39,7 +39,7 @@ if [ ! -d "$OUTPUTDIR" ]; then
 fi
 
 echo "Backing up sites and config"
-tar -zcf "$TEMP_SITES_BACKUP_DIR/sites.tar.gz" $INPUT_DIR
+tar -zcfh "$TEMP_SITES_BACKUP_DIR/sites.tar.gz" $INPUT_DIR
 tar -zcf "$TEMP_NGINX_CONFIG_DIR/configs.tar.gz" "/etc/nginx/sites-available"
 
 echo "Backing up databases"
